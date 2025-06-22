a7fde81feee19b0c7bcb4f312558685f79af4da2
Dpbm <dpbm136@gmail.com>
2025-06-22 12:53:48 -0300

    fixed pokemon routing

--- src/app/cards/card.component.ts
+++ src/app/cards/card.component.ts
@@ -1,16 +1,17 @@
 import { Component, Input } from '@angular/core';
 import { PokemonCard } from '@utils/pokemon';
-import { IonIcon, IonImg } from '@ionic/angular/standalone';
+import { IonIcon, IonImg, IonButton } from '@ionic/angular/standalone';
 import { heartOutline } from 'ionicons/icons';
 import { addIcons } from 'ionicons';
 import { PokemonId } from '@customTypes/pokemon';
 import { FavoriteCallback } from '../home/services/callback.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.scss'],
-  imports: [IonIcon, IonImg],
+  imports: [IonIcon, IonImg, IonButton, RouterModule],
 })
 export class CardComponent {
   @Input() pokemon!: PokemonCard;
