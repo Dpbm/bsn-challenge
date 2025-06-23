1da1d6ad421fb17e90e8d4ed64899abaaaa1c433
Dpbm <dpbm136@gmail.com>
2025-06-23 17:00:20 -0300

    refactor navigation service

--- src/app/services/navigation.service.ts
+++ src/app/services/navigation.service.ts
@@ -4,18 +4,31 @@ import { LoginComponent } from '../login/login.component';
 
 @Injectable({ providedIn: 'root' })
 export class NavigationService {
-  private nav!: IonNav;
+  private nav!: IonNav; // uses the root nav
 
   setNav(nav: IonNav) {
+    /**
+     * Sets the nav component instance.
+     */
     this.nav = nav;
   }
 
-  async goToLogin() {
+  goToLogin() {
+    /**
+     * Clear everything and go directly to login page.
+     */
     this.nav.popToRoot().then((success) => {
       if (!success) {
         return;
       }
       this.nav.push(LoginComponent);
     });
+  }
+
+  goToHome() {
+    /**
+     * Push on stack the home page (root).
+     */
+    this.nav.push('/');
   }
 }
