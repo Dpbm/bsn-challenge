23b6039b18185fbc4976848cbdecf0b7b9068718
Dpbm <dpbm136@gmail.com>
2025-06-23 16:58:37 -0300

    refactor home component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -31,6 +31,7 @@ import {
 import { Favorite } from '../services/pokemons/favorite.service';
 import { LoginComponent } from '../login/login.component';
 import { UserComponent } from '../user/user.component';
+import { from } from 'rxjs';
 
 @Component({
   selector: 'home',
@@ -58,13 +59,19 @@ import { UserComponent } from '../user/user.component';
   ],
 })
 export class HomeComponent implements OnInit {
-  private currentOffset: number = 0;
+  private currentOffset: number = 0; // offset for pagination
+
   pokemons: PokemonCard[] = [];
   favorites: Set<PokemonId> = new Set();
-  isLogged: boolean = !!this.supabase.session;
 
+  isLogged: boolean = false;
+
+  /* used for handling the navigation to pokemon details page
+  using that, after returning to home, we can check the data  
+  about this specific pokemon. */
   selectedPokemon: PokemonId | null = null;
 
+  // Components for routing
   loginComponent = LoginComponent;
   userComponent = UserComponent;
 
@@ -75,6 +82,11 @@ export class HomeComponent implements OnInit {
   ) {}
 
   private getPokemons() {
+    /**
+     * Get pokemons data from PokeAPI.
+     * Once it uses the http client from angular, it returns an observable that
+     * must be subscribed.
+     */
     this.fetcher.fetch(this.currentOffset).subscribe({
       next: (pokemons: PokemonCard[]) => {
         this.pokemons = [...this.pokemons, ...pokemons];
@@ -83,61 +95,120 @@ export class HomeComponent implements OnInit {
     });
   }
 
-  private async getFavorites() {
-    const data = await this.supabase.getFavoritePokemons(this.currentOffset);
-    this.favorites = new Set(data);
+  private getFavorites() {
+    /**
+     * We use the supabase client to fetch the favorites from our postgres database.
+     * It returns a promise with the pokemons IDs.
+     */
+
+    this.supabase
+      .getFavoritePokemons(this.currentOffset)
+      .then(
+        (pokemons: PokemonId[]) =>
+          (this.favorites = new Set([
+            ...Array.from(this.favorites),
+            ...pokemons,
+          ]))
+      );
   }
 
-  private async getData() {
-    this.getFavorites().then(() => {
-      this.getPokemons();
-    });
+  private getData() {
+    /**
+     * Fetch all pokemons related data at once.
+     */
+    this.getFavorites();
+    this.getPokemons();
   }
 
-  async ngOnInit() {
-    this.isLogged = await this.supabase.isLogged();
-    await this.getData();
+  ngOnInit() {
+    this.supabase
+      .isLogged()
+      .then((isLogged: boolean) => (this.isLogged = isLogged));
+    this.getData();
   }
 
   onIonInfinite(event: InfiniteScrollCustomEvent) {
+    /**
+     * Used for infinite scroll component.
+     */
+
     this.getData();
+
     setTimeout(() => {
+      /**
+       * Ensures that the scroll event is
+       * hit only once after so time.
+       * It help us to avoid duplicated requests.
+       */
+
       event.target.complete();
     }, 500); // debounce
   }
 
-  async favoriteHandler(event: FavoriteEventData) {
-    const success = await this.favoriteCallback.call(
-      event.pokemonId,
-      event.wasFavorite
-    );
+  favoriteHandler(event: FavoriteEventData) {
+    /**
+     * The handler for favorite calls.
+     * It's a callback for clicking on favorite button.
+     *
+     * Using this one, we can track which cards were saved as favorite
+     * by the user.
+     */
 
-    if (!success) return;
+    this.favoriteCallback
+      .call(event.pokemonId, event.wasFavorite)
+      .then((success: boolean) => {
+        if (!success) return;
 
-    if (event.wasFavorite) {
-      this.favorites.delete(event.pokemonId);
-      return;
-    }
+        if (event.wasFavorite) {
+          this.favorites.delete(event.pokemonId);
+          return;
+        }
 
-    this.favorites.add(event.pokemonId);
+        this.favorites.add(event.pokemonId);
+      });
   }
 
   isFavorite(id: PokemonId): boolean {
+    /**
+     * Helper function that cards use to
+     * know which pokemon is on favorites list.
+     */
     return this.favorites.has(id);
   }
 
   navigationHandler(event: CardNavigationEvent) {
+    /**
+     * Handler to check which pokemon the user
+     * has click to see details.
+     */
+
     this.selectedPokemon = event.pokemonId;
   }
 
   ionViewDidEnter() {
+    /**
+     * The event raised after coming back from some page.
+     * If the selected pokemon is not null, we check if his favorite status
+     * has changed.
+     */
     if (!this.selectedPokemon) return;
 
+    const pokemonId: PokemonId = this.selectedPokemon;
+
     this.supabase.isFavoritePokemon(this.selectedPokemon).then((favorite) => {
-      if (favorite && !this.favorites.has(this.selectedPokemon!)) {
-        this.favorites.add(this.selectedPokemon!);
-      } else if (!favorite && this.favorites.has(this.selectedPokemon!)) {
-        this.favorites.delete(this.selectedPokemon!);
+      if (favorite && !this.favorites.has(pokemonId)) {
+        /**
+         * Since we know that this pokemon wasn't in favorites list
+         * and now its status says that his a favorite, we need to add it to our
+         * local list.
+         */
+        this.favorites.add(pokemonId);
+      } else if (!favorite && this.favorites.has(pokemonId)) {
+        /**
+         * Now, once it not a favorite pokemon and it's in the list, we need
+         * to remove it from there.
+         */
+        this.favorites.delete(pokemonId);
       }
     });
   }
