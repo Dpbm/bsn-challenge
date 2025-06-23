4e5ed745b83342c18036766d8f2f982e40bab648
Dpbm <dpbm136@gmail.com>
2025-06-23 16:58:20 -0300

    refactor guards

--- src/app/guards/is-logged.guard.ts
+++ src/app/guards/is-logged.guard.ts
@@ -1,38 +1,28 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  GuardResult,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { CanActivate, GuardResult } from '@angular/router';
 import { SupabaseService } from '../services/supabase.service';
 import { Injectable } from '@angular/core';
+import { NavigationService } from '../services/navigation.service';
 
 @Injectable({ providedIn: 'root' })
 export class isLoggedGuard implements CanActivate {
   constructor(
     private readonly supabase: SupabaseService,
-    private router: Router
+    private navigation: NavigationService
   ) {}
 
-  async canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Promise<GuardResult> {
+  canActivate(): Promise<GuardResult> {
     /**
      * Checks supabase session. If it exists, the user is logged
      * so, he can continue his navigation.
      *
-     * @returns {Promise<bool>}
+     * @returns {Promise<bool>} - True if he's logged
      */
 
     return this.supabase.isLogged().then((logged: boolean) => {
-      if (logged) {
-        return true;
+      if (!logged) {
+        this.navigation.goToLogin();
       }
-
-      this.router.navigate(['/login']);
-      return false;
+      return logged;
     });
   }
 }
--- src/app/guards/is-not-logged.guard.ts
+++ src/app/guards/is-not-logged.guard.ts
@@ -1,38 +1,29 @@
-import {
-  ActivatedRouteSnapshot,
-  CanActivate,
-  GuardResult,
-  Router,
-  RouterStateSnapshot,
-} from '@angular/router';
+import { CanActivate, GuardResult } from '@angular/router';
 import { SupabaseService } from '../services/supabase.service';
 import { Injectable } from '@angular/core';
+import { NavigationService } from '../services/navigation.service';
 
 @Injectable({ providedIn: 'root' })
 export class isNotLoggedGuard implements CanActivate {
   constructor(
     private readonly supabase: SupabaseService,
-    private router: Router
+    private navigation: NavigationService
   ) {}
 
-  async canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot
-  ): Promise<GuardResult> {
+  canActivate(): Promise<GuardResult> {
     /**
      * Checks supabase session. If it exists, the user is logged
      * so, he must not access some areas (like login page).
      *
-     * @returns {Observable<bool>}
+     * @returns {Promise<bool>} - True if user is not logged
      */
 
     return this.supabase.isLogged().then((logged: boolean) => {
-      if (!logged) {
-        return true;
+      if (logged) {
+        this.navigation.goToHome();
       }
 
-      this.router.navigate(['/']);
-      return false;
+      return !logged;
     });
   }
 }
