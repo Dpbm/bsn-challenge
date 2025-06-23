680cff80636ab94995491ec584c868138e4e35d6
Dpbm <dpbm136@gmail.com>
2025-06-23 16:58:05 -0300

    refactor card component

--- src/app/cards/card.component.ts
+++ src/app/cards/card.component.ts
@@ -33,6 +33,11 @@ export class CardComponent {
   }
 
   favorite() {
+    /**
+     * Emit an event when user clicks on favorite button.
+     * This event is meant to be a callback to his parent component. This way
+     * We don't need to inject any state provider for each pokemon card.
+     */
     this.favoriteEvent.emit({
       pokemonId: this.pokemon.id,
       wasFavorite: this.isFavorite,
@@ -40,6 +45,11 @@ export class CardComponent {
   }
 
   navigate() {
+    /**
+     * This method, emits an event when user clicks on the pokemon card
+     * to navigate to details page. This way, we can track which pokemon the user
+     * interacted with, and then updated states as we need.
+     */
     this.navigateEvent.emit({
       pokemonId: this.pokemon.id,
     });
